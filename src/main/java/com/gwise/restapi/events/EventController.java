2147d0e5d268762752c9aeff39a8b3f46e7f89e6
김기정 <bangry313@naver.com>
2020-02-13 17:55:44 +0900

    44

--- src/main/java/com/gwise/restapi/events/EventController.java
+++ src/main/java/com/gwise/restapi/events/EventController.java
@@ -23,13 +23,11 @@ import com.gwise.restapi.common.ErrorsResource;
 @Controller
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
 public class EventController {
-	private final EventRepository eventRepository;	
+	private final EventRepository eventRepository;
 	private final ModelMapper modelMapper;
 	private final EventValidator eventValidator;
-	
-	public EventController(EventRepository eventRepository, 
-			ModelMapper modelMapper, 
-			EventValidator eventValidator) {
+
+	public EventController(EventRepository eventRepository, ModelMapper modelMapper, EventValidator eventValidator) {
 		this.eventRepository = eventRepository;
 		this.modelMapper = modelMapper;
 		this.eventValidator = eventValidator;
@@ -40,30 +38,28 @@ public class EventController {
 		return ResponseEntity.ok(this.eventRepository.findAll(pageable));
 	}
 
-	
 	@PostMapping
-	public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto,
-			Errors errors) {
-		//EventDto 제약조건 에러
-		//한번에 에러 메시지를 출력하려면 주석처리한다.
-		if(errors.hasErrors()) {
-			return badRequest(errors);
-		}		
-		//입력필드 로직체크
-		eventValidator.validate(eventDto, errors);
-		
-		if(errors.hasErrors()) {
+	public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
+		// EventDto 제약조건 에러
+		// 한번에 에러 메시지를 출력하려면 주석처리한다.
+		if (errors.hasErrors()) {
 			return badRequest(errors);
 		}
-		
-		Event event = modelMapper.map(eventDto, Event.class); 
-		//free, offline 필드값 처리
+		// 입력필드 로직체크
+		eventValidator.validate(eventDto, errors);
+
+		if (errors.hasErrors()) {
+			return badRequest(errors);
+		}
+
+		Event event = modelMapper.map(eventDto, Event.class);
+		// free, offline 필드값 처리
 		event.update();
 		Event addEvent = this.eventRepository.save(event);
 
 		ControllerLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(addEvent.getId());
 		URI createUri = selfLinkBuilder.toUri();
-		
+
 		EventResource eventResource = new EventResource(event);
 		eventResource.add(linkTo(EventController.class).withRel("query-events"));
 		eventResource.add(selfLinkBuilder.withRel("update-event"));
@@ -72,7 +68,7 @@ public class EventController {
 	}
 
 	private ResponseEntity badRequest(Errors errors) {
-		//return ResponseEntity.badRequest().body(errors); // .build();
+		// return ResponseEntity.badRequest().body(errors); // .build();
 		return ResponseEntity.badRequest().body(new ErrorsResource(errors));
 	}
 }
